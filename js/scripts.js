ba1ae0bb2a0eb367afa22229c2caf4c490155cb6
Matthias Leuhof <matthias.leuhof@hotmail.com>
2017-05-21 20:54:35 +0200

    Updated the map functions.

--- js/scripts.js
+++ js/scripts.js
@@ -1,42 +1,75 @@
-// my scripts
 
+// VARIABLES
+var map;
+var route;
 
+// WALKS
 var barhopping = {
   start: [52.377297, 4.898783],
   end: [52.357487, 4.885650]
 }
+var theBucketList = {
+  start: [52.369385, 4.914318],
+  end: [52.381070, 4.871317]
+}
+var foodAndChinese = {
+  start: [52.373891, 4.883762],
+  end: [52.356072, 4.900242]
+}
+var floraAndFauna = {
+  start: [52.366188, 4.913374],
+  end: [52.356491, 4.862562]
+}
+var grafitti = {
+  start: [52.366712, 4.865824],
+  end: [52.356386, 4.910799]
+}
 
-function initMap() {
+// MAIN SCRIPTS
+function main() {
 
-  var map = new GMaps({
+  map = new GMaps({
     div: '#map',
     lat: 52.368722,
     lng: 4.896982,
     zoom: 14,
-    draggable: false
+    draggable: false,
+    scrollwheel: false
   })
 
-  $('#start').on('click', function() {
-    map.travelRoute({
-      origin: barhopping.start,
-      destination: barhopping.end,
-      travelMode: 'walking',
-      step: function(e) {
-        $('#instructions').append('<li>' + e.instructions + '</li>');
-        $('#instructions li:eq(' + e.step_number + ')').delay(450 * e.step_number).fadeIn(200, function() {
-          map.drawPolyline({
-            path: e.path,
-            strokeColor: '#ff0000',
-            strokeOpacity: 1,
-            strokeWeight: 6
-          });
-        });
-      }
-    })
+  map.getRoutes({
+    origin: barhopping.start,
+    destination: barhopping.end,
+    travelMode: 'walking',
+    callback: function(e){
+      route = new GMaps.Route({
+        map: map,
+        route: e[0],
+        strokeColor: '#ff0000',
+        strokeOpacity: 0.5,
+        strokeWeight: 10
+      });
+    }
   });
 
-  // map = new google.maps.Map(document.getElementById('map'), {
-  //   center: {lat: 52.368722, lng: 4.896982},
-  //   zoom: 14
-  // });
+  $('#next-stop').click(function(e){
+    e.preventDefault();
+    route.forward();
+
+    console.log(route);
+
+    if(route.step_count < route.steps_length)
+      $('#instructions').append('<li>'+route.steps[route.step_count].instructions+'</li>');
+  });
+
+  $('#previous-stop').click(function(e){
+    e.preventDefault();
+    route.back();
+
+    if(route.step_count >= 0)
+      $('#instructions').find('li').last().remove();
+  });
+
 }
+
+$(document).ready(main);
